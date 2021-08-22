11f63521949d0eaa8143184dfa070d22bc23d43a
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-08-22 10:05:47 +0300

    5. Переопределить и equals и hashCode [#1002 #192376]

--- src/main/java/ru/job4j/map/Calendar.java
+++ src/main/java/ru/job4j/map/Calendar.java
@@ -1,4 +1,6 @@
 package ru.job4j.map;
 
+import java.util.Date;
+
 public class Calendar {
 }
--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,5 +1,10 @@
 package ru.job4j.map;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
 public class User {
     private String name;
     private int children;
@@ -9,5 +14,36 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    public static void main(String[] args) {
+        Calendar calendar = new Calendar();
+        User userIvan = new User("Ivan", 10, calendar);
+        User userIvanM = new User("Ivan", 10, calendar);
+
+        HashMap<User, Object> users = new HashMap<>();
+        users.put(userIvan, new Object());
+        users.put(userIvanM, new Object());
+
+        for (User user : users.keySet()) {
+            System.out.println(user.hashCode());
+        }
     }
 }
