dabedf948ff3abe17aec4f25bd0f1a6e5456b555
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-08-28 09:40:16 +0300

    0.1. FileOutputStream. [#252488 #192888]

--- src/main/java/ru/job4j/io/Matrix.java
+++ src/main/java/ru/job4j/io/Matrix.java
@@ -3,27 +3,28 @@ package ru.job4j.io;
 import java.io.FileOutputStream;
 
 public class Matrix {
-    static int[][] multiple(int size) {
-        int[][] matrix = new int[size][size];
+    static String multiple(int size) {
+        StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                matrix[i][j] = (i + 1) * (j + 1);
+                Integer value = (i + 1) * (j + 1);
+                String strValue = value.toString();
+                stringBuilder.append(strValue.length() == 1
+                        ? " ".concat(strValue).concat(" ") : strValue.concat(" "));
             }
+            stringBuilder.append(System.lineSeparator());
         }
-        return matrix;
+        return stringBuilder.toString();
     }
 
     public static void main(String[] args) {
 
-        int[][] table = Matrix.multiple(9);
+        String str = Matrix.multiple(9);
 
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            for (int[] x : table) {
-                for (int y : x) {
-                    String s = String.valueOf(y);
-                    s = s.length() == 1 ? " ".concat(s).concat(" ") : s.concat(" ");
-                    out.write(s.getBytes());
-                }
+            String[] lines = str.split(System.lineSeparator());
+            for (String line : lines) {
+                out.write(line.getBytes());
                 out.write(System.lineSeparator().getBytes());
             }
         } catch (Exception e) {
