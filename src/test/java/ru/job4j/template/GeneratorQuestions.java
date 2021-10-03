81e477499f31eee916511c031028297d6346b0d5
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-10-03 12:13:17 +0300

    3. Шаблонизатор. [#855 #211606]

--- src/test/java/ru/job4j/template/GeneratorQuestions.java
+++ src/test/java/ru/job4j/template/GeneratorQuestions.java
@@ -5,7 +5,7 @@ import java.util.Map;
 public class GeneratorQuestions implements Generator {
     @Override
     public String produce(String template, Map<String, String> args) {
-        if (args.get("arg1") != null){
+        if (args.get("arg1") != null) {
             throw new IllegalArgumentException();
         }
         return "I am Ivan";
