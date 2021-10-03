417719799b2cf7f51462f27e86845f71e52ec7bd
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-10-03 12:11:11 +0300

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorQuestions.java
@@ -0,0 +1,13 @@
+package ru.job4j.template;
+
+import java.util.Map;
+
+public class GeneratorQuestions implements Generator {
+    @Override
+    public String produce(String template, Map<String, String> args) {
+        if (args.get("arg1") != null){
+            throw new IllegalArgumentException();
+        }
+        return "I am Ivan";
+    }
+}
---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.template;
+
+import org.junit.Test;
+import ru.job4j.kiss.MaxMin;
+import ru.job4j.tdd.Ticket3D;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class GeneratorTest {
+
+    @Test
+    public void produce() {
+        Generator gen = new GeneratorQuestions();
+        String templ = "I am ${name}?";
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Ivan");
+        String result = gen.produce(templ, args);
+        assertThat(result, is("I am Ivan"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void produceEx() {
+        Generator gen = new GeneratorQuestions();
+        String templ = "I am ${name}?";
+        Map<String, String> args = new HashMap<>();
+        args.put("arg1", "Ivan");
+        String result = gen.produce(templ, args);
+    }
+}
